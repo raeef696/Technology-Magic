178d7757eaee0548ec27f3d07e8f6347585af9bb
raeef696 <75561536+raeef696@users.noreply.github.com>
2022-04-25 13:36:36 -1100

    first

---
+++ assets/js/myjs.js
@@ -0,0 +1,18 @@
+
+
+
+window.addEventListener("scroll", function(){
+  var header = document.querySelector("header");
+ header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+
+
+
+
+
+
+
+
+
