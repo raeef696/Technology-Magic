b98b4428b019de101909a080873760fabfbbea7d
raeef696 <75561536+raeef696@users.noreply.github.com>
2022-05-25 12:02:22 -1100

    new

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -14,5 +14,21 @@ window.addEventListener("scroll", function(){
 
 
 
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+
 
 
